55423d53b97066442fb3e522528316e1998eacf8
Lívia Qian <qianlivia@gmail.com>
2020-05-25 23:44:21 +0200

    Add new figures

--- src/generate_log.py
+++ src/generate_log.py
@@ -20,13 +20,20 @@ scales = [4, 1]
 plot_multiple_diagrams(file_names, labels, title, "img/small_bach_38_4_without_scaling.png", scales=scales)
 
 #############
-# Scarletti #
+# Scarlatti #
 #############
 
 file_names = ["logs/layers/run-version_0-tag-loss.csv",
                 "logs/layers/run-version_23-tag-loss.csv",
                 "logs/layers/run-version_24-tag-loss.csv"]
 labels = ["6 layers", "1 layer", "3 layers"]
-title = "Training loss for the Scarletti dataset for different models with loss scaling"
+title = "Training loss for the Scarlatti dataset for different models with loss scaling"
 scales = [1, 1, 1]
-plot_multiple_diagrams(file_names, labels, title, "img/scarletti_layers.png", scales=scales, N=10)
+plot_multiple_diagrams(file_names, labels, title, "img/scarlatti_layers.png", scales=scales, N=10)
+
+file_names = ["logs/hidden/run-version_27-tag-loss.csv",
+                "logs/hidden/run-version_29-tag-loss.csv"]
+labels = ["Embedding: 600, hidden: 900", "Embedding: 800, hidden: 1200"]
+title = "Training loss for the Scarlatti dataset with different sizes and loss scaling"
+scales = [1, 1]
+plot_multiple_diagrams(file_names, labels, title, "img/scarlatti_hidden.png", scales=scales, N=10)
